8df90704958be00b8cf7baf0b1b58b0741eb1e8f
IkuseTohki <four.seasons.haru@gmail.com>
2025-12-22 23:32:59 +0900

    fix(ui): フィルタ解除時に選択行が画面外になる問題を修正
    フィルタの適用・解除などの操作でログリストの表示内容が変更された際、選択中の行が表示範囲外になってしまう問題を修正。
    DataGridScrollToSelectionBehavior に AutoScrollOnCollectionChanged 添付プロパティを追加し、ItemsSourceのコレクション変更イベントを検知して選択行へ自動スクロールする機能を実装。
    
    - DataGridScrollToSelectionBehavior.cs: コレクション変更監視機能を追加
    - MainWindow.xaml: DataGrid に上記 Behavior を適用

--- MultiLogViewer/Behaviors/DataGridScrollToSelectionBehavior.cs
+++ MultiLogViewer/Behaviors/DataGridScrollToSelectionBehavior.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Specialized;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -5,6 +7,8 @@ namespace MultiLogViewer.Behaviors
 {
     public static class DataGridScrollToSelectionBehavior
     {
+        #region SelectedItem (Existing)
+
         public static readonly DependencyProperty SelectedItemProperty =
             DependencyProperty.RegisterAttached(
                 "SelectedItem",
@@ -29,5 +33,112 @@ namespace MultiLogViewer.Behaviors
                 dataGrid.ScrollIntoView(e.NewValue);
             }
         }
+
+        #endregion
+
+        #region AutoScrollOnCollectionChanged (New)
+
+        public static readonly DependencyProperty AutoScrollOnCollectionChangedProperty =
+            DependencyProperty.RegisterAttached(
+                "AutoScrollOnCollectionChanged",
+                typeof(bool),
+                typeof(DataGridScrollToSelectionBehavior),
+                new PropertyMetadata(false, OnAutoScrollOnCollectionChangedChanged));
+
+        public static bool GetAutoScrollOnCollectionChanged(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(AutoScrollOnCollectionChangedProperty);
+        }
+
+        public static void SetAutoScrollOnCollectionChanged(DependencyObject obj, bool value)
+        {
+            obj.SetValue(AutoScrollOnCollectionChangedProperty, value);
+        }
+
+        // Private attached property to store the handler delegate to avoid leaks and allow unsubscription
+        private static readonly DependencyProperty CollectionChangedHandlerProperty =
+            DependencyProperty.RegisterAttached(
+                "CollectionChangedHandler",
+                typeof(NotifyCollectionChangedEventHandler),
+                typeof(DataGridScrollToSelectionBehavior),
+                new PropertyMetadata(null));
+
+        private static void OnAutoScrollOnCollectionChangedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is DataGrid dataGrid)
+            {
+                if ((bool)e.NewValue)
+                {
+                    dataGrid.Loaded += DataGrid_Loaded;
+                    dataGrid.Unloaded += DataGrid_Unloaded;
+                }
+                else
+                {
+                    dataGrid.Loaded -= DataGrid_Loaded;
+                    dataGrid.Unloaded -= DataGrid_Unloaded;
+                    UnsubscribeFromCollection(dataGrid);
+                }
+            }
+        }
+
+        private static void DataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (sender is DataGrid dataGrid)
+            {
+                SubscribeToCollection(dataGrid);
+            }
+        }
+
+        private static void DataGrid_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (sender is DataGrid dataGrid)
+            {
+                UnsubscribeFromCollection(dataGrid);
+            }
+        }
+
+        private static void SubscribeToCollection(DataGrid dataGrid)
+        {
+            // Ensure we don't subscribe twice
+            UnsubscribeFromCollection(dataGrid);
+
+            if (dataGrid.Items is INotifyCollectionChanged collection)
+            {
+                // Create a handler that captures the dataGrid instance
+                NotifyCollectionChangedEventHandler handler = (s, args) =>
+                {
+                    // Action needs to be deferred to let the grid update its internal state
+                    dataGrid.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        if (dataGrid.SelectedItem != null)
+                        {
+                            try
+                            {
+                                dataGrid.ScrollIntoView(dataGrid.SelectedItem);
+                            }
+                            catch
+                            {
+                                // Ignore errors if item is not in view or grid is in invalid state
+                            }
+                        }
+                    }), System.Windows.Threading.DispatcherPriority.ContextIdle);
+                };
+
+                collection.CollectionChanged += handler;
+                dataGrid.SetValue(CollectionChangedHandlerProperty, handler);
+            }
+        }
+
+        private static void UnsubscribeFromCollection(DataGrid dataGrid)
+        {
+            var handler = (NotifyCollectionChangedEventHandler)dataGrid.GetValue(CollectionChangedHandlerProperty);
+            if (handler != null && dataGrid.Items is INotifyCollectionChanged collection)
+            {
+                collection.CollectionChanged -= handler;
+                dataGrid.ClearValue(CollectionChangedHandlerProperty);
+            }
+        }
+
+        #endregion
     }
 }
